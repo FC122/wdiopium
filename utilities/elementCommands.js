8e30671bf8d1f0ce933542607ca4c544001cc7e9
Filip CIca <72666124+FC122@users.noreply.github.com>
2024-02-11 11:35:43 +0100

    Create elementCommands.js

---
+++ utilities/elementCommands.js
@@ -0,0 +1,9 @@
+/**
+ * https://webdriver.io/docs/api/browser/addCommand 
+ * File for adding helper commands on element level
+ * Command is accessible in tests and screens on $ object
+ * e.g. $(some_id).getUser()
+ * */
+module.exports = {
+    examplECommand: () => console.log('Example Command')
+}
