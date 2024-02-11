4a615e3a738466fd93fc6ce5e35d61bc2ff0a769
Filip CIca <72666124+FC122@users.noreply.github.com>
2024-02-11 11:32:53 +0100

    Create apiCommands.js

---
+++ utilities/apiCommands.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+/**
+ * https://webdriver.io/docs/api/browser/addCommand 
+ * File for adding commands that interact with the server
+ * Command is accessible in tests and screens on browser object
+ * e.g. browser.getUser()
+ * */
+module.exports = {
+    exampleCommand: () => {/**Custom Implementation */ }
+}
