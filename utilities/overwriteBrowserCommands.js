1da3e98f3a02a64e4036435f114e8b0a9598d5aa
Filip CIca <72666124+FC122@users.noreply.github.com>
2024-02-11 11:37:35 +0100

    Create overwriteBrowserCommands.js

---
+++ utilities/overwriteBrowserCommands.js
@@ -0,0 +1,9 @@
+/**
+ * https://webdriver.io/docs/api/browser/overwriteCommand/
+ * File for overwriting existing commands accessible on the browser object
+ * Command is accessible in tests and screens on browser object
+ * e.g. browser.pause()
+ * */
+module.exports = {
+    exampleCommand: (originalFn) => {/**Custom Implementation */ }
+}
